0bf5b0004d8eedb8f9a56265bb55eed95e9bb046
ange stephane <boguhestephane97@gmail.com>
2023-05-24 16:08:22 +0100

    add filtering and sorting by depth * Create constructor with 3 parameters * Create constructor with 1 parameter * Use satisfie interface to filter depth between two values * Use sortingBy interface to sorting depth * Use getName interface to get the filter name that we use

---
+++ src/main/java/org/stephane/filter/FilterAndSortingByDepth.java
@@ -0,0 +1,50 @@
+package org.stephane.filter;
+
+import org.stephane.entity.Seisme;
+
+import java.util.ArrayList;
+
+public class FilterAndSortingByDepth implements FilterAndSorting {
+    double minDepth;
+    double maxDepth;
+    String nameFilter;
+
+    public FilterAndSortingByDepth(double minDepth, double maxDepth, String name ) {
+        this.minDepth = minDepth;
+        this.maxDepth = maxDepth;
+        this.nameFilter = name;
+    }
+
+    public FilterAndSortingByDepth(String name){
+        this.nameFilter = name;
+    }
+
+    @Override
+    public boolean satisfie(Seisme se){
+        if(this.maxDepth >= se.getDepth() &&  se.getDepth() >= this.minDepth){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public ArrayList<Seisme> sortingBy(ArrayList<Seisme> datas) {
+        Seisme tmp;
+        for(int i = 0; i< datas.size(); i++){
+            for(int k = i+1; k < datas.size(); k++) {
+                if(datas.get(i).getDepth() > datas.get(k).getDepth()){
+                    tmp = datas.get(i);
+                    datas.set(i, datas.get(k));
+                    datas.set(k, tmp);
+                }
+            }
+        }
+        return datas;
+    }
+
+    @Override
+    public String getName() {
+        return this.nameFilter;
+    }
+
+}
